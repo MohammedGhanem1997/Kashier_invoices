d1c9b65e6c39c14319ea4c1131e55cfcb62c849e
MohammedGhanem1997 <102333174+MohammedGhanem1997@users.noreply.github.com>
2022-09-25 19:52:16 +0200

    Update UserSeeder.php
    Signed-off-by: MohammedGhanem1997 <102333174+MohammedGhanem1997@users.noreply.github.com>

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Admin;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Rinvex\Subscriptions\Models\PlanFeature;
 
 class UserSeeder extends Seeder
 {
@@ -30,7 +31,23 @@ class UserSeeder extends Seeder
         $user->password = Hash::make('12345678');
         $user->save();
 
+        $plan = app('rinvex.subscriptions.plan')->create([
+            'name' => 'Pro',
+            'description' => 'Pro plan',
+            'price' => 90,
+            'signup_fee' => 1.99,
+            'invoice_period' => 1,
+            'invoice_interval' => 'month',
+            'trial_period' => 15,
+            'trial_interval' => 'day',
+            'sort_order' => 1,
+            'currency' => 'USD',
+        ]);
 
+// Create multiple plan features at once
+        $plan->features()->saveMany([
+            new PlanFeature(['name' => 'period', 'value' => 90, 'sort_order' => 10, 'resettable_period' =>3, 'resettable_interval' => 'month']),
+        ]);
 
 
     }
